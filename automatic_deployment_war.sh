1ea2726c28f3a13d991dda3848175902eb1f56c5
bluevitality <inmoonlight@163.com>
2017-04-28 11:11:11 +0800

    automatic_deployment_war.sh
    待完善

---
+++ automatic_deployment_war.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#war包全自动部署脚本(非热部署)
+#by inmoonlight@163.com
+
+#脚本第一个参数是war包的名字，脚本与war应在同一目录下
+war_full_path=$(pwd -P)/$1
+
+#判断是否输入了war包的名字
+if [ ! -n "$1" ] ;then
+    echo "you have not input war fiilename!"
+	exit 1
+fi
+
+#tomcat启动与关闭的路径及webapps路径
+tom_up=/usr/local/tomcat/bin/startup.sh
+tom_down=/usr/local/tomcat/bin/shutdown.sh
+tom_webapps=/usr/local/tomcat/webapps
+
+#关闭tom
+function stop() {
+	eval "$tom_down"
+	sleep 2
+	eval "$tom_down"
+	#killall java 	#慎用
+}
+
+#部署war包
+function deploy() {
+	#按时间创建临时的旧包备份文件
+	mkdir -p /tmp/$(date "+%F_%H:%M") || echo "The created directory already exists : /tmp/$(date "+%F_%H:%M")"
+	cp "${tom_webapps}/$1" /tmp/$(date "+%F_%H:%M")  &> /dev/null || echo "The file does not exist, the first deployment : $1"
+	#移除webapps下的旧包之后再部署
+	rm -rf "${tom_webapps}/$1" "${tom_webapps}/${1%%\.war}" || echo "mv old file fail..."
+	mv "${war_full_path}" ${tom_webapps} && "Successful deployment..."
+
+#启动tom
+function start() {
+	eval "$tom_up" && echo "tom to start"
+}
+
+#run
+stop
+deploy
+start
