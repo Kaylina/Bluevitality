996f81b97ef21ab1375e682080ea364ee5ce36f3
bluevitality <inmoonlight@163.com>
2017-04-28 13:35:58 +0800

    Update automatic_deployment_war.sh

--- automatic_deployment_war.sh
+++ automatic_deployment_war.sh
@@ -3,12 +3,12 @@
 #by inmoonlight@163.com
 
 #脚本第一个参数是war包的名字，脚本与war应在同一目录下
-war_full_path=$(pwd -P)/$1
+war_full_path="$(pwd -P)/$1"
 
 #判断是否输入了war包的名字
-if [ ! -n "$1" ] ;then
-	echo "you have not input war fiilename!"
-	exit 1
+if [[ ! -n "$1" ]] || [[ ! -e "$(pwd -P)/$1" ]] ;then
+        echo "you have not input war fiilename!"
+        exit 1
 fi
 
 #tomcat启动与关闭的路径及webapps路径
@@ -18,27 +18,28 @@ tom_webapps=/usr/local/tomcat/webapps
 
 #关闭tom
 function stop() {
-	eval "$tom_down"
-	sleep 2
-	eval "$tom_down"
-	#killall java 	#慎用
+        eval "$tom_down"
+        sleep 2
+        #eval "$tom_down"
+        killall java    #慎用(避免对其他java应用产生影响)
 }
 
 #部署war包
 function deploy() {
-	#按时间创建临时的旧包备份文件
-	mkdir -p /tmp/$(date "+%F_%H:%M") || echo "The created directory already exists : /tmp/$(date "+%F_%H:%M")"
-	cp "${tom_webapps}/$1" /tmp/$(date "+%F_%H:%M")  &> /dev/null || echo "The file does not exist, the first deployment : $1"
-	#移除webapps下的旧包之后再部署
-	rm -rf "${tom_webapps}/$1" "${tom_webapps}/${1%%\.war}" || echo "mv old file fail..."
-	mv "${war_full_path}" ${tom_webapps} && "Successful deployment..."
+        #按时间创建临时的旧包备份文件
+        mkdir -p /tmp/$(date "+%F_%H:%M") || echo "The created directory already exists : /tmp/$(date "+%F_%H:%M")"
+        cp "${tom_webapps}/${1}" /tmp/$(date "+%F_%H:%M")  &> /dev/null || echo "The file does not exist, the first deployment : $1"
+        #移除webapps下的旧包之后再部署
+        rm -rf "${tom_webapps}/${1}" "${tom_webapps}/${1%%\.war}" || echo "mv old file fail..."
+        mv "${war_full_path}" ${tom_webapps} && echo "Successful deployment..."
+}
 
 #启动tom
 function start() {
-	eval "$tom_up" && echo "tom to start"
+        eval "$tom_up"
 }
 
 #run
 stop
-deploy
+deploy $1 #参数传递(war包名)
 start
