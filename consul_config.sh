a33ad6f22a700b52c61e7bb28ff23bf3724328e4
bluevitality <inmoonlight@163.com>
2017-05-13 18:53:16 +0800

    再完善一下，并加入备注

--- consul_config.sh
+++ consul_config.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 #SERVER VARIABLES
-	service_number=1							#服务端数量
-	data_path=/var/consul						#数据存放目录
+	service_number=1					#服务端数量
+	data_path=/var/consul					#数据存放目录
 	conf_path=/etc/consul.d					#配置存放目录（服务注册信息&健康检测脚本）
 	bind_add=192.168.10.1					#consul只能绑定在一个IP上
-	dc_name=default								#数据中心名字
+	dc_name=default						#数据中心名字
 
 #CLIENT VARIABLES
-	cluster_node_ip=192.168.10.1			#必须定义（集群中任意节点的IP地址）
+	cluster_node_ip=192.168.10.1				#必须定义（集群中任意节点的IP地址）
 	client_bind_ip=192.168.10.2				#必须定义（自身绑定的地址）
-	register_serv=web								#服务名称
-	register_port=80								#服务端口
-	register_tages="my_info"					#用户自定义tag信息
+	register_serv=web					#服务名称
+	register_port=80					#服务端口
+	register_tages="my_info"				#用户自定义tag信息
 	register_check_interval=10				#健康检查间隔，second
-	register_check_scripts="ping -c 1 127.0.0.1 2>&1"			#脚本/命令的返回值非0则不健康
-	kv_storage_enable="on"												#是否启用键值存储功能（on/off）
-	kv_storage_serv=${register_serv}		#
-	kv_storage_key="123"						#键
+	register_check_scripts="ping -c 1 127.0.0.1 2>&1"	#脚本/命令的返回值非0则不健康
+	kv_storage_enable="on"					#是否启用键值存储功能（on/off）
+	kv_storage_serv=${register_serv}			#
+	kv_storage_key="123"					#键
 	kv_storage_value="321"					#值
-	dc_name=${dc_name}						#请保持默认
+	dc_name=${dc_name}					#请保持默认
 	data_path=${data_path}					#
 	conf_path=${conf_path}					#
 
@@ -43,18 +43,18 @@ function agent_server() {
 	}
 
 	consul_command="consul  \
-    agent  \
-    -server  \
-    -rejoin  \
-    -bootstrap-expect ${service_number}  \
-    -pid-file=/var/run/consul_server.pid  \
-    -datacenter ${dc_name}  \
-    -data-dir=${data_path:=/var/consul}  \
-    -config-dir=${conf_path:=/etc/consul}  \
-    -node=${node_name}  \
-    -bind=${bind_add}  \
-    -client 0.0.0.0  \
-    -syslog"
+    	agent  \
+    	-server  \
+    	-rejoin  \
+    	-bootstrap-expect ${service_number}  \
+    	-pid-file=/var/run/consul_server.pid  \
+    	-datacenter ${dc_name}  \
+    	-data-dir=${data_path:=/var/consul}  \
+    	-config-dir=${conf_path:=/etc/consul}  \
+    	-node=${node_name}  \
+    	-bind=${bind_add}  \
+    	-client 0.0.0.0  \
+    	-syslog"
 
 	[[ "${service_number}" == "1" ]] && consul_command=$( echo ${consul_command} | sed 's/-bootstrap-expect.../-bootstrap /' )
 	
@@ -84,14 +84,14 @@ function  register_service() {
 					\"${register_tages:=user_info}\"
 				]
 			}
-		}" > ${conf_path}/${register_serv}.json	#服务注册&健康检查信息写到配置目录，文件格式 .json
+		}" > ${conf_path}/${register_serv}.json		#服务注册&健康检查信息写到配置目录，文件格式 .json
 		
 }
 
 function kv_storage() {
 
 	curl -X PUT -d  "${kv_storage_value}" http://localhost:8500/v1/kv/${kv_storage_serv}/${kv_storage_key}
-	echo -e "\033[32mabout KV info .... please look over:\ncurl http://${client_bind_ip}:8500/v1/kv/${kv_storage_serv}/${kv_storage_key}\033[0m"
+	echo -e "\033[32mabout KV info :\ncurl http://${client_bind_ip}:8500/v1/kv/${kv_storage_serv}/${kv_storage_key}\033[0m"
 	
 }
 
@@ -104,25 +104,25 @@ function agent_client() {
 	}
 	
 	consul_command="consul  \
-    agent  \
-    -ui  \
-    -node=${node_name}  \
-    -pid-file=/var/run/consul_client.pid  \
-    -bind=${client_bind_ip}  \
-    -datacenter=${dc_name}  \
-    -data-dir=${data_path}  \
-    -config-dir=${conf_path}  \
-    -join ${cluster_node_ip}  \
+    	agent  \
+    	-ui  \
+    	-node=${node_name}  \
+    	-pid-file=/var/run/consul_client.pid  \
+    	-bind=${client_bind_ip}  \
+    	-datacenter=${dc_name}  \
+    	-data-dir=${data_path}  \
+    	-config-dir=${conf_path}  \
+    	-join ${cluster_node_ip}  \
 	-client 0.0.0.0  \
-    -rejoin   \
-    -syslog"
+    	-rejoin   \
+    	-syslog"
 	
 	register_service  ;  echo -e "\033[32mregister_service finish...\nRun command: \n ${consul_command}\033[0m"
 	
 	sed -i "/consul/d" /etc/rc.local
 	
 	eval "nohup $consul_command &> /var/log/consul-client.log &" && \
-	echo $consul_command >> /etc/rc.local  ||  {
+	echo $consul_command >> /etc/rc.local || {
 		echo -e "\033[31mconsul client start fail......\033[0m"
 	}
 	
@@ -150,7 +150,7 @@ function script_variables_check() {
 	&& [[ ${#register_tages} -ge 1  &&	${#register_check_interval} -ge 1 ]]  \
 	&& [[ ${#register_check_scripts} -ge 1 && ${#kv_storage_enable} -ge 1 ]]  \
 	&& [[ "${kv_storage_enable}" == "on" || "${kv_storage_enable}" == "off" ]]  \
-    || 	{ 
+   	|| { 
 		echo -e "\033[31mscript Client variables check fail ....\033[0m" 
 		exit 1  
 	}
@@ -182,18 +182,16 @@ echo
 case ${vstr} in 
 	"i") 	consul_install
 	;;
-	"u")  consul_remove
+	"u")  	consul_remove
 	;;
 	"s") 	agent_server
 	;;
 	"c") 	agent_client
 	;;
-	*)  exit 1
+	*)  	exit 1
 	;;
 esac
 
-#	客户端的查询??（提供用户read参数来查看）
-#	如何让consul后台运行并保持输出到一个文件中...
 #
 #设置模板：
 #	模板要查询的服务端地址
@@ -204,5 +202,3 @@ esac
 #如果周末来得及：（先不考虑和consul结合的太紧密的场景，仅使用）
 #	docker的swarm集群的初始化环境配置和网络配置
 #	注：脚本方式....
-#	
-#--------------------------------------------------------------------------------------------------------------
