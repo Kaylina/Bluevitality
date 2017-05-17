8402a33d1c21ecd7cfd43296fdba5990794602fe
bluevitality <inmoonlight@163.com>
2017-05-17 23:07:10 +0800

    Update Docker_Tomcat.sh

--- Docker_Tomcat.sh
+++ Docker_Tomcat.sh
@@ -12,7 +12,7 @@ docker rm -f $(docker ps -a | awk '/tomcat/{print $1}') 2> /dev/null
 #创建资源目录（先清空资源目录数据，防止多次执行起冲突）
 rm -rf /data/docker-tomcat-data/{webapps,config,logs}
 mkdir -p /data/docker-tomcat-data/{webapps,config,logs} 
-chcon -Rt svirt_sandbox_file_t /data/docker-tomcat-data/{webapps,config,logs} 2> /dev/null
+chcon -Rt svirt_sandbox_file_t /data/docker-tomcat-data/{webapps,config,logs} 2>&-
 setenforce 0 && chmod 766 -R /data/docker-tomcat-data/{webapps,config,logs}
 
 #移动war包
@@ -28,3 +28,8 @@ docker.io/tomcat:8.0.43-jre8
 #注意：
 #暂时取消了挂载：
 #-v /data/docker-tomcat-data/config:/usr/local/tomcat/conf \
+
+#结构修改
+#限制容器可使用的内存，Tomcat容器的堆大小（百度）
+#增加使用注释方法
+#
