f84b92e822167f7939d55de9d5555305250f99c9
bluevitality <inmoonlight@163.com>
2017-05-18 11:04:24 +0800

    Add files via upload

---
+++ Docker/tomcat.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#tomcat: 版本：tomcat:8.0.43-jre8、需要先从官网下载tomcat镜像
+#脚本执行时：WAR包、镜像需要在脚本同一目录下(流程：设置变量、导入镜像、挂载目录、运行容器)
+#挂载的目录默认在：/data/docker-tomcat
+
+
+HOST_TOM_PORT=8089      #Tomcat与宿主机关联的端口
+CONTAINER_NAME=tomcat   #容器名
+WAR_NAME=signage.war    #WAR包名
+
+#若不存在则导入镜像
+docker images | grep -q  "docker.io/tomcat" ||  docker load < tomcat.8.0.43-jre8.tar
+
+#若已存在此容器先删除（本脚本没有使用dockerfile方式）
+docker rm -f $(docker ps -a | awk "/${CONTAINER_NAME}/{print $1}") 2>&-
+
+#创建资源目录（先清空资源目录数据，防止多次执行起冲突）
+rm -rf /data/docker-tomcat/{webapps,config,logs}
+mkdir -p /data/docker-tomcat/{webapps,config,logs} 
+chcon -Rt svirt_sandbox_file_t /data/docker-tomcat/{webapps,config,logs} 2>&-
+setenforce 0 && chmod 766 -R /data/docker-tomcat/{webapps,config,logs}
+
+#移动war包
+cp -rf ${WAR_NAME} /data/docker-tomcat/webapps
+
+#运行容器
+docker run -d -p ${HOST_TOM_PORT}:8080 --name $CONTAINER_NAME \
+-v /data/docker-tomcat/webapps:/usr/local/tomcat/webapps -v /data/docker-tomcat/logs:/usr/local/tomcat/logs \
+docker.io/tomcat:8.0.43-jre8
+
+#------------------------------------------------------------------
+# 注意：
+# 暂时取消了挂载：
+# -v /data/docker-tomcat-data/config:/usr/local/tomcat/conf \
+ 
+# 结构修改
+# 限制容器可使用的内存，Tomcat容器的堆大小（百度）
+#
