fafb862d1dd8e1ed0ded72bc288f00b3a3689992
bluevitality <inmoonlight@163.com>
2017-05-21 03:15:43 +0800

    模板需要根据具体环境写

--- 服务发现/consul_config.sh
+++ 服务发现/consul_config.sh
@@ -8,29 +8,36 @@
 #最后一个重要的部分：模板！！！
 
 #SERVER VARIABLES
-	service_number=1					#consul服务端数量（高可用）
+	service_number=1					#consul服务端数量(高可用)
 	data_path=/var/consul					#数据存放目录
-	conf_path=/etc/consul.d					#配置存放目录（服务注册信息&健康检测脚本）
-	bind_add=192.168.126.135				#consul只能绑定在1个IP（集群node间通讯）
+	conf_path=/etc/consul.d					#配置存放目录(服务注册信息&健康检测脚本)
+	bind_add=192.168.126.135				#consul只能绑定在1个IP(集群node间通讯)
 	dc_name=default						#数据中心命名
 
 #CLIENT VARIABLES
-	cluster_node_ip=192.168.126.135				#必须定义（集群中任意节点的IP地址）加入集群..
-	client_bind_ip=192.168.126.161				#必须定义（client自身绑定的地址）
+	cluster_node_ip=192.168.126.135				#必须定义(集群中任意节点的IP地址)加入集群..
+	client_bind_ip=192.168.126.161				#必须定义(client自身绑定的地址)
 	register_serv=web					#服务名称
 	register_port=80					#服务端口
-	register_tages="/var/www/html"			     	#自定义tag信息（建议用于模板进行配置渲染）
+	register_tages="/var/www/html"			     	#自定义tag信息(建议用于模板进行配置渲染)
 	register_check_interval=10				#健康检查间隔，秒
-	register_check_scripts="ping -c 1 127.0.0.1 2>&1"	#脚本/命令的返回值，非0则不健康（此检查对应当前服务，注意json）
-        register_check_timeout=5                                #脚本执行超时时间（秒）
+	register_check_scripts="ping -c 1 127.0.0.1 2>&1"	#脚本/命令的返回值，非0则不健康(此检查对应当前服务，注意json)
+        register_check_timeout=5                                #脚本执行超时时间(秒)
         register_check_notes="XXX service check fail"           #脚本检查失败提示信息
 	kv_storage_enable="on"					#是否启用键值存储功能：on/off
 	kv_storage_serv=${register_serv}			#
 	kv_storage_key="123"					#键
 	kv_storage_value="321"					#值
-	dc_name=${dc_name}					#请保持默认（与服务端在同1个数据中心）
+	dc_name=${dc_name}					#请保持默认(与服务端在同1个数据中心)
 	data_path=${data_path}					#
 	conf_path=${conf_path}					#
+
+#TEMPLATE VARIABLES
+	enable-template="off"					#是否启用模板功能：on/off (可从命令行选项执行)
+	only-out-to-view="yes"					#是否不渲染模板仅输出到屏幕：yes/no
+	consul_template=/etc/consul.d/consul_template.templ	#服务模板读入路径(需要事先定义好模板!).....
+	confige_file=/etc/nginx/nginx.conf			#服务配置文件输出路径
+	template_exec="nginx -s reload"				#执行的命令(如重启服务)
 
 #check user
 [ "$(id -u)" != "0" ] && { echo -e "\033[1;31mError: must be root to run this script.\033[0m" ; exit 1 ; }
@@ -40,9 +47,14 @@ function consul_install() {
 	[[ -x /usr/local/bin/consul ]] ||  { 
 		cp ./consul /usr/local/bin  &&  chmod a+x /usr/local/bin/consul
 		mkdir -p ${conf_path}  ${data_path}  &&  rm -rf ${conf_path:=protect"/"}/*  ${data_path:=protect"/"}/*
-	}  2>&-
+	}  
 	
-}
+	[[ -x /usr/local/bin/consul-template ]] || {
+		cp ./consul-template /usr/local/bin && chmod a+x /usr/local/bin/consul
+		mkdir -p ${consul_template%/*} ${confige_file%/*} && rm -rf ${consul_template%/*}/*
+	}
+	
+} 2>&-
 
 function agent_server() {
 
@@ -166,36 +178,45 @@ function script_variables_check() {
 	&& [[ ${#dc_name} -ge 0 ]]  \
 	&& echo ${bind_add} | grep -Eq "[0-9]{2,}\.[0-9]+\.[0-9]+\.[0-9]+" \
 	|| {
-		echo -e "\033[31mscript Service variables check fail ....\033[0m" ; exit 1
+		echo -e "\033[31mScript Server variables check fail .... \n \033[0m" ; exit 1
 	}	
 	
 	#CLIENT VARIABLES CHECK
 	echo ${cluster_node_ip} | grep -Eq "[0-9]{2,}\.[0-9]+\.[0-9]+\.[0-9]+"  \
 	&& echo ${client_bind_ip} | grep -Eq "[0-9]{2,}\.[0-9]+\.[0-9]+\.[0-9]+"  \
+	&& echo ${kv_storage_enable} | grep -Eq "on|off" \
 	&& echo ${register_check_timeout} | grep -Eq "^[[:digit:]]+$"  \
 	&& [[ ${#register_serv} -ge 1  &&  ${#register_port} -ge 2 ]]  \
 	&& [[ ${#register_tages} -ge 1  &&  ${#register_check_interval} -ge 1 ]]  \
 	&& [[ ${#register_check_scripts} -ge 1  &&  ${#kv_storage_enable} -ge 1 ]]  \
-	&& [[ "${kv_storage_enable}" == "on" || "${kv_storage_enable}" == "off" ]]  \
    	|| { 
-		echo -e "\033[31mscript Client variables check fail ....\033[0m" ; exit 1  
+		echo -e "\033[31mScript Client variables check fail .... \n \033[0m" ; exit 1  
 	}
 	
 	#CHECK KV STORAGE
 	if [[ "${kv_storage_enable}" != "on" ]] ; then
 		if [["${kv_storage_enable}" != "off"]] ; then
-			echo -e "\033[31mscript KV variables check fail .... \n \033[0m" 
+			echo -e "\033[31mScript KV variables check fail .... \n \033[0m" 
 			exit 1 
 		fi
 	fi
+	
+	#CONSUL TEMPLATE CHECK
+	echo "${enable-template}" | grep -Eq "on|off" \
+	&& echo "${only-out-to-view}" | grep -Eq "yes|no" \
+	&& [[ ${#consul_template} -ge 6 && ${#confige_file} -ge 3 ]] \
+	&& [[ ${#template_exec} -ge 2 ]] \
+	|| {
+		echo -e "\033[31mConsul-Temp.. variables check fail .... \n \033[0m" ; exit 1
+	}
+	
 }
 
 function consul_remove() {
 
-	killall consul 
+	killall consul consul-template
 	sed -i '/consul.*/d'  /etc/rc.local
-	rm -rf /usr/local/bin/consul ${conf_path:=protect"/"} ${data_path:=protect"/"}
-	rm -rf /var/{run,log}/consul_*
+	rm -rf /usr/local/bin/consul /var/{run,log}/consul_* ${conf_path:=protect} ${data_path:=protect} ${consul_template%/*}
 	
 } 2>&-
 
@@ -265,10 +286,32 @@ function consul_help() {
 
 }
 
-#First check the variables , after the start.....
-script_variables_check && read -p "isnatll(i) uninstall(u) server(s) client(c) more_help(h)：" -t 8
+function template-exec() {
 
-echo
+	consul members | grep -qF "server"  || { 
+		echo -e "\033[31mconsul_template start fail...[0m"
+		exit 1
+	}
+	
+	exec-command="nohuop consul-template -consul-addr 127.0.0.1:8500 -retry 15s -syslog -log-level=info \
+	-template "${consul_template}:/${confige_file}:${template_exec}" &> /var/log/consul-template.log &"
+	
+	if [[ "${only-out-to-view}" == "no" ]] ;then
+		eval $( echo $exec-command | sed -n 's/&.*/-dry/p' )
+	else
+		eval $exec-command
+	fi
+	
+	[[ "$?" == "0" ]] && echo -e "\033[32m\nconsul-template start success...\033[0m" || exit 1
+}
+
+[[ "${enable-template}" == "on" ]] && template-exec
+
+#First check the variables , after the start.....
+script_variables_check && echo -en "\033[1;34mIsnatll(i) Uninstall(u) Server(s) Client(c) More_help(m)：\033[0m"
+
+read -t 8
+
 case ${REPLY} in 
 	"i") 	consul_install
 	;;
@@ -280,8 +323,10 @@ case ${REPLY} in
 	;;
         "q")    consul leave            #离开集群
         ;;
-        "h")    consul_help             #查询...
+        "m")    consul_help             #查询...
         ;;
+	"t")	template-exec		#模板...
+	;;
 	*)  	exit 1
 	;;
 esac
@@ -296,9 +341,33 @@ esac
 #       查某服务所有节点             	curl http://10.245.6.90:8500/v1/health/service/<service>
 #       查某状态所有检查          	curl http://10.245.6.90:8500/v1/health/state/<state>
 
-#	加密：S端生成密钥：consul keygen ----> 执行时c/s均追加参数：consul ..... -encrypt=XXXXXXX
+#	加密：
+#	S端生成密钥：consul keygen ----> 执行时c/s均追加参数：consul ..... -encrypt=XXXXXXX
 
-
+#	模板：
+#	{{range service "web"}}		注：第一个range的范围相对于URL，即此相对于：http://127.0.0.1:8500/v1/catalog/services下的内容
+#	{{.Name}}:{{.Address}}:{{.Port}} --- {{.Tags}}
+#	{{end}}
+#	
+#	{{ if keyExists "app/beta_active" }}	如果指定的key存在则......
+#	.......
+#	{{ else }}
+#	.......
+#	{{ end }}
+#
+#	{{ range ls "service/redis" }}		环绕key？
+#	{{ .Key }}:{{ .Value }}{{ end }}
+#
+#	{{ range services }}			输出：node01 tag1,tag2,tag3
+#	{{ .Name }}: {{ .Tags | join "," }}{{ end }}
+#
+#	{{ base64Decode "aGVsbG8=" }}		hello
+#	{{ base64Encode "hello" }}		aGVsbG8=
+#	
+#	{{ range services }}
+#	{{ range service .Name }}
+#	{{ end }}
+#	{{ end }}
 
 
 
