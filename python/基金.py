fbc12ba8de07b286de3f82dfb974e36a50e5475a
bluevitality <inmoonlight@163.com>
2017-08-08 14:17:07 +0800

    Update 基金.py

--- python/基金.py
+++ python/基金.py
@@ -2,49 +2,60 @@
 
 import requests
 import multiprocessing
-import re
+from flask import Flask,render_template,request,redirect,url_for
+from werkzeug.utils import secure_filename
 import os
+import re
 import time
 from bs4 import  BeautifulSoup
 import sqlite3
 
+time_value=[]
 db_name=str(time.strftime('sqlite.'+'%Y-%m',time.localtime(time.time()))+'.db')
 conn = sqlite3.connect(db_name)
+
+app = Flask(__name__)
 
 def createdb(name):
     try:
         conn = sqlite3.connect(name)
         conn.execute('''CREATE TABLE record(Name CHAR(10),Value CHAR(50),nTime CHAR(50));''')
         conn.close()
-    except:
-        print "already exists %s file..." %(name)
-    else:
-        print "DB name: \t %s" %(str(os.getcwd())+'\\'+name)
+    except: print "already exists %s file..." %(name)
+    else:   print "DB name: \t %s" %(str(os.getcwd())+'\\'+name)
 
 def html_info(url,name,times=300):
-    try:
+    global time_value
+    while True:
+        if str(time.strftime('%H:%S',time.localtime(time.time()))) == "15:02": 
+            os.exit()
         html_string=BeautifulSoup(requests.get(str(url)).text,'html.parser').select("#gz_gszzl")
         v=re.compile(r'(?<=>).*%').findall(str(html_string))[0]
-    except:
-        print 'notice!... %s' %('catch fail...')
-    t=time.strftime('%Y-%m-%d:%H:%M',time.localtime(time.time()))
-    print "%s \t %s \t %s" %(name,v,t)
-    sql="INSERT INTO record (Name,Value,nTime) VALUES ('%s','%s','%s')" %(name,v,t)
-    conn.execute(sql)
-    conn.commit()
-    time.sleep(times)
-        
-delay_time=300
+        t=time.strftime('%Y-%m-%d:%H:%M',time.localtime(time.time()))
+        time_value.append({'time':t,'value':v})
+        print "%s \t %s \t %s" %(name,v,t)
+        sql="INSERT INTO record (Name,Value,nTime) VALUES ('%s','%s','%s')" %(name,v,t)
+        conn.execute(sql)
+        conn.commit()
+        time.sleep(times)
+
+@app.route('/',methods=['GET'])
+def index():
+    global time_value
+    return render_template("show.html",digit=time_value)    #包含时间和数值的列表字典！键是time
+
+delay_time=1
 Address={}
 Address['创金合信资源股票发起式C']='http://fund.eastmoney.com/003625.html?spm=search'
 Address['招商中证白酒指数分级']='http://fund.eastmoney.com/161725.html?spm=search'
 
 if __name__ == '__main__':
     createdb(name=db_name)
-    pool = multiprocessing.Pool(processes = len(Address))
-    while True:
-        for key,value in Address.items():
-            pool.apply_async(html_info, (value,key,delay_time))
-            if str(time.strftime('%H:%S',time.localtime(time.time()))) == "15:02":
-                conn.close()
-                os.exit()
+    pool = multiprocessing.Pool(processes=len(Address))
+    for key,value in Address.items():
+        print "Name: %-20s  URL:%s" %(key,value)
+        pool.apply_async(html_info, (value,key,delay_time))
+    app.run(host="0.0.0.0",debug=True)
+    pool.close()
+    pool.join()
+
