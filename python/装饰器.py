ccdce307025d3a6a299a8f0840fd4f47cdb2f3f8
bluevitality <inmoonlight@163.com>
2017-08-13 23:07:24 +0800

    Update 装饰器.py

--- python/装饰器.py
+++ python/装饰器.py
@@ -1,29 +1,29 @@
 #encoding:utf-8
 
-def log(f):
+def log(func):
     def wrapper(*args,**kw):
-        print u"无参装饰器..."
-        return f(*args,**kw)
+        print "Testing..."
+        return func(*args,**kw)
     return wrapper
 
 @log
-def a(x,y):
+def function(x,y):
     print x*y
 
-a(3,2)  #输出6
+function(10,10)    #输出100
 
 #---------------------------------------------------
 
-def xy(v):
-    def wrapper1(f):
-        def wrapper2(*args,**kw):
-            print "装饰器参数:%s" %(v)
-            return f(*args,**kw)
-        return wrapper2
-    return wrapper1
+def log(argument):
+    def wrapper_func(func):
+        def wrapper_args(*args,**kw):
+            print argument
+            return func(*args,**kw)
+        return wrapper_args
+    return wrapper_func
 
-@xy('test')
-def b(x,y):
+@log('Testing...')
+def function(x,y):
     print x*y
 
-b(3,6)  #输出18
+function(10,10)     #输出100
