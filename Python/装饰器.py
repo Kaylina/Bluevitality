6e8b6d3bb7a0374d3133078275bb194e59d788d1
bluevitality <inmoonlight@163.com>
2017-10-14 15:10:14 +0800

    Update 装饰器.py

--- Python/装饰器.py
+++ Python/装饰器.py
@@ -44,7 +44,34 @@ def now():
 
 now()
 
+#---------------------------------------------------
+
+#在类的外部定义一个类的装饰器对类方法进行装饰并使其能够调用类中的其他方法
+def catch_exception(origin_func):
+    def wrapper(self, *args, **kwargs):
+        try:
+            u = origin_func(self, *args, **kwargs)
+            return u
+        except Exception:
+            self.revive() #不用顾虑，直接调用原来的类的方法
+            return 'an Exception raised.'
+    return wrapper
+
+
+class Test(object):
+    def __init__(self):
+        pass
+
+    def revive(self):
+        print('revive from exception.')
+        # do something to restore
+
+    @catch_exception
+    def read_value(self):
+        print('here I will do something.')
+        # do something.
+        
 """
-关于装饰器的具体概念，参见：
+关于装饰器的具体概念：
 http://www.liaoxuefeng.com/wiki/001374738125095c955c1e6d8bb493182103fac9270762a000/001386819879946007bbf6ad052463ab18034f0254bf355000
 """
