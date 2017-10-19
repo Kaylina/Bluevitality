244743f446bd1d2b5650df9a7e3d18c993d78f7a
bluevitality <inmoonlight@163.com>
2017-10-19 14:44:52 +0800

    Create 百度翻译.py

---
+++ Python/Request/百度翻译.py
@@ -0,0 +1,22 @@
+#coding=utf-8
+
+import requests
+import json
+
+def py():
+    url = 'http://fanyi.baidu.com/v2transapi/'
+    data={
+      'from':'en',
+      'to':'zh',
+      'query':"captain",      #单词
+      'transtype':'translang',
+      'simple_means_flag':'3'
+      }        
+    headers ={'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36 SE 2.X MetaSr 1.0'}
+    response = requests.post(url,data,headers=headers)
+    head = response.headers
+    x=response.json()['trans_result']['data'][0]
+    print json.dumps(x).decode("unicode-escape")
+
+if __name__=="__main__":
+    py()
