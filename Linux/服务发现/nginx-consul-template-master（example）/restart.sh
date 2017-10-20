f699f10654983c279f157b486189d1780acf57d6
bluevitality <inmoonlight@163.com>
2017-10-20 18:28:10 +0800

    fix consul

--- Linux/服务发现/nginx-consul-template-master（example）/restart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-NGINX=/usr/sbin/nginx
-NGINX_CONF=/etc/nginx/nginx.conf
-PID=/var/run/nginx.pid
-
-echo "DEBUG: restarting nginx"
-# gracefully restart nginx
-${NGINX} -c ${NGINX_CONF} -t && \
-kill -s HUP $(cat ${PID})
