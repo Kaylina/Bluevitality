e9f612bd889446606799e17ba09885044d61f384
bluevitality <inmoonlight@163.com>
2017-10-29 23:54:36 +0800

    Update 跟踪网站变动.sh

--- Linux/脚本/跟踪网站变动.sh
+++ Linux/脚本/跟踪网站变动.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
-# 用途 跟踪网页更新
+# 跟踪网页更新
 
+#检查参数
 if [$# -ne 1]:
 then
-  echo -e "$ Usage: $0 URL\n"
-  exit 1;
+	  echo -e "$ Usage: $0 URL\n"
+	  exit 1;
 fi
 
+#定义跟踪次数
 first_time=0
 
 if [! -e "last.html"]:
 then
-  first_time=1
+	  first_time=1
 fi
 
+#下载指定页面
 curl --silent $1 -o recent.html
 if [$first_time -ne 1];
 then
-  changes=$(diff -u last.html recent.html)
-  if [-n "$changes"];
-  then
-    echo -e "changes"\n"
-    echo "$changes"
-  else
-    echo -e "\nWebsite has no changes"
-  fi
+	#比较文件内容检（检查）
+	changes=$(diff -u last.html recent.html)
+	if [-n "$changes"];
+	  then
+			echo -e "changes \n $changes"
+	  else
+			echo -e "\nWebsite has no changes"
+	fi
 else
-  echo "[First run] Archiving.."
+	echo "[First run] Archiving.."
 fi
+
 cp recent.html last.html
