6800100edb63230ea5c3286bb990e89f6b2e1b71
bluevitality <inmoonlight@163.com>
2017-10-30 22:11:15 +0800

    Update rsyncInstall.sh

--- Linux/数据共享&存储相关/Rsync + Inotity/setupShell/rsyncInstall.sh
+++ Linux/数据共享&存储相关/Rsync + Inotity/setupShell/rsyncInstall.sh
@@ -6,64 +6,46 @@ if [ $(id -u) != "0" ]; then
     exit 1
 fi
 
-printf "\n"
-printf "==========================\n"
-printf " Rsync v3.1.0 Install	  \n"
-printf " copyright:www.doitphp.com \n"
-printf "==========================\n"
-printf "\n\n"
-
-if [ ! -s websrc ]; then    
-    mkdir websrc
-    printf "Folder:websrc has been created.\n\n"
-fi
-
-cd websrc
-
-printf "\n========= source package download start =========\n\n"
-
-if [ -s rsync-3.1.1.tar.gz ]; then
-    echo "rsync-3.1.1.tar.gz [found]"
-else
-    echo "rsync-3.1.1.tar.gz download now..."
-    wget http://rsync.samba.org/ftp/rsync/rsync-3.1.1.tar.gz	
-fi
-
+#检查源码包是否存在
 if [ ! -f rsync-3.1.1.tar.gz ]; then
     printf "Error: rsync-3.1.1.tar.gz not found!\n"
     exit 1
 fi
 
+#若已存在解压的源码目录先删除
 if [ -s rsync-3.1.1 ]; then
     rm -rf rsync-3.1.1
 fi
+
+#解压安装
+printf "\nInstall Rsync ...\n\n"
 tar zxvf rsync-3.1.1.tar.gz
-
-printf "\n========= source package download completed =========\n\n"
-
-printf "========= Rsync install start... =========\n\n"
-
 cd rsync-3.1.1
 ./configure --prefix=/usr/local/rsync
 make
 make install
 cd -
 
+#检查是否存在可执行文件
 if [ ! -f /usr/local/rsync/bin/rsync ]; then
     printf "Error: rsync compile install failed!\n"
     exit 1
 fi
 
+#创建配置目录和日志目录
 mkdir -p /usr/local/rsync/etc
 mkdir -m 0777 -p /usr/local/rsync/logs
 
-echo "#username:password">/usr/local/rsync/etc/rsyncd.pass
+#创建认证文件
+echo "#username:password" > /usr/local/rsync/etc/rsyncd.pass
 chmod 600 /usr/local/rsync/etc/rsyncd.pass
 
+#检查若有配置文件先备份
 if [ -s /usr/local/rsync/etc/rsyncd.conf ]; then
     mv /usr/local/rsync/etc/rsyncd.conf /usr/local/rsync/etc/rsyncd.conf.bak
 fi
 
+#导入配置
 cat >/usr/local/rsync/etc/rsyncd.conf<<EOF
 uid = nobody
 gid = nobody
@@ -85,22 +67,23 @@ hosts deny=*
 #gid = root
 #path = /rsync module path
 #auth users = username
-
 #read only = no 
+
 EOF
 
 rsync --daemon --config=/usr/local/rsync/etc/rsyncd.conf
 
+#写入启动文件
 mv ../rsync.rcd.txt /etc/init.d/rsyncd
 chmod 0755 /etc/init.d/rsyncd
 
+#检查是否开机自启，若不存在则写入/etc/rc.local
 isSet=`grep "/usr/local/rsync/bin/rsync --daemon" /etc/rc.local | wc -l`
 if [ "$isSet" == "0" ]; then
     echo "/usr/local/rsync/bin/rsync --daemon --config=/usr/local/rsync/etc/rsyncd.conf">>/etc/rc.local
 fi
+
 service rsyncd restart
 
-printf "\n========== Rsync install Completed! =======\n\n"
+printf "\nRsync is running ...\n\n"
 ps aux | grep rsync | grep -v "grep"
-cat /etc/rc.local
-printf "============== The End. ==============\n"
