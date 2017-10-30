f9a6f157678d576e8bceeb64851e279f0b56fc5b
bluevitality <inmoonlight@163.com>
2017-10-30 22:21:39 +0800

    Update inotify.sh

--- Linux/数据共享&存储相关/Rsync + Inotity/setupShell/inotify.sh
+++ Linux/数据共享&存储相关/Rsync + Inotity/setupShell/inotify.sh
@@ -6,38 +6,12 @@ if [ $(id -u) != "0" ]; then
     exit 1
 fi
 
-printf "\n"
-printf "============================\n"
-printf " inotify-tools v3.13 Install\n"
-printf " copyright:www.doitphp.com  \n"
-printf "============================\n"
-printf "\n\n"
-
-if [ ! -s websrc ]; then    
-    mkdir websrc
-    printf "Folder:websrc has been created.\n\n"
-fi
-
-cd websrc
-
-printf "\n========= source package download start =========\n\n"
-
-if [ -s inotify-tools-3.13.tar.gz ]; then
-    echo "inotify-tools-3.13.tar.gz [found]"
-else
-    echo "inotify-tools-3.13.tar.gz download now..."
-    wget http://nchc.dl.sourceforge.net/project/inotify-tools/inotify-tools/3.13/inotify-tools-3.13.tar.gz
-fi
-
 if [ -s inotify-tools-3.13 ]; then
     rm -rf inotify-tools-3.13
 fi
+
+printf "\nInotify Install ...\n\n"
 tar zxvf inotify-tools-3.13.tar.gz
-
-printf "\n========= source package download completed =========\n\n"
-
-printf "========= Inotify tools install start... =========\n\n"
-
 cd inotify-tools-3.13
 ./configure --prefix=/usr/local
 make
@@ -49,5 +23,5 @@ if [ ! -f /usr/local/lib/libinotifytools.so ]; then
     exit 1
 fi
 
-printf "\n========== Inotify tools install Completed! =======\n\n"
-printf "============== The End. ==============\n"
+printf "\n===== Inotify tools install Completed! ====\n\n"
+
