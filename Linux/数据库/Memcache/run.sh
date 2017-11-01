1bf7c6ef19f33a3260f9f4a117d55d858a5f4d7c
bluevitality <inmoonlight@163.com>
2017-11-01 21:16:58 +0800

    Update run.sh

--- Linux/数据库/Memcache/run.sh
+++ Linux/数据库/Memcache/run.sh
@@ -1,26 +1,9 @@
 #!/bin/bash
 #
-# memcached - this script starts and stops the memcached daemon
-#
-# chkconfig: - 85 15
-# description: Memcached is an in-memory key-value store for small chunks of arbitrary data (strings, objects) from results of database calls, 
-# 			   API calls, or page rendering.
 # processname: memcached
 # config: /etc/sysconfig/memcached
 # pidfile: /var/run/memcached.pid
 
-# Source function library.
-. /etc/rc.d/init.d/functions
-
-if [ -f /etc/sysconfig/memcached ]; then
-	. /etc/sysconfig/memcached
-fi
-
-# Source networking configuration.
-. /etc/sysconfig/network
-
-# Check that networking is up.
-[ "$NETWORKING" = "no" ] && exit 0
 
 MEMCACHED_HOME="/usr/local/memcached"
 memcached=$MEMCACHED_HOME"/bin/memcached"
@@ -32,37 +15,47 @@ PORT=11211
 MAX_MEMORY=1024
 MAX_CONNEXTIONS=1024
 MAX_REQUESTS=100
-
 RETVAL=0
 
+
+# Source function library.
+. /etc/rc.d/init.d/functions
+
+if [ -f /etc/sysconfig/memcached ]; then
+	. /etc/sysconfig/memcached
+fi
+
+# Source networking configuration.
+. /etc/sysconfig/network
+
 start() {
-    [ -x $memcached ] || exit 5
-    echo -n $"Starting $PROG: "
-    ${memcached} -p ${PORT} -u ${USER} -m ${MAX_MEMORY} -c ${MAX_CONNEXTIONS} -R ${MAX_REQUESTS} -P ${pidfile} -d start
-    RETVAL=$?
-    echo
-    [ $RETVAL -eq 0 ] && touch $lockfile
-    return $RETVAL
+    	[ -x $memcached ] || exit 5
+    	echo -n $"Starting $PROG: "
+    	${memcached} -p ${PORT} -u ${USER} -m ${MAX_MEMORY} -c ${MAX_CONNEXTIONS} -R ${MAX_REQUESTS} -P ${pidfile} -d start
+    	RETVAL=$?
+    	echo
+    	[ $RETVAL -eq 0 ] && touch $lockfile
+    	return $RETVAL
 }
 
 stop() {
 	echo -n $"Stopping $PROG: "
 	killproc $memcached -QUIT
 	RETVAL=$?
-    echo
-    [ $RETVAL = 0 ] && rm -f ${lockfile} ${pidfile}
+    	echo
+    	[ $RETVAL = 0 ] && rm -f ${lockfile} ${pidfile}
 }
 
 case "$1" in
-    start)
-        start
+    	start)
+        	start
 		;;
-	reload|restart)
-        stop
-        start
+    	reload|restart)
+        	stop
+        	start
 		;;
-    stop)
-        stop
+    	stop)
+        	stop
 		;;
 	condrestart|try-restart)
 		if status >&/dev/null; then
@@ -78,7 +71,7 @@ case "$1" in
 		$memcached --help
 		RETVAL=$?
 		;;
-    *)
+    	*)
 		echo $"Usage: $PROG {start|reload|restart|condrestart|try-restart|stop|status|help}"
 		RETVAL=2
 		;;
